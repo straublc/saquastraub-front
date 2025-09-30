576cb402aaae77ff4682a25c7046ad4171b44d60
straublc <straublc@gmail.com>
2025-09-30 11:20:00 -0300

    ajuste vercel-2

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, 
-});
-
 const app = createApp(App);
 
 app.use(router);
 
-app.config.globalProperties.$axios = api;
-
-app.mount('#app');
+app.config.globalProperties.$axios = axios;
+;
