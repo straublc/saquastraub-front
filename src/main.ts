c908706233659c7758a0508ed53a46221dfc85ad
straublc <straublc@gmail.com>
2025-09-30 11:01:25 -0300

    Ajuste para vercel

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,20 @@
-// main.ts
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router'; 
+import router from './router';
 import axios from 'axios';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL, 
+});
+
 const app = createApp(App);
 
 app.use(router);
-app.config.globalProperties.$axios = axios;
+
+app.config.globalProperties.$axios = api;
 
 app.mount('#app');
