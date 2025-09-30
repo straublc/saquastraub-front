c6964e6733fac4d59b2cc792d8a00e6952b0575a
straublc <straublc@gmail.com>
2025-09-29 23:06:10 -0300

    att

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
+axios.defaults.baseURL = process.env.VUE_APP_API_URL || 'http://localhost:3000';
+
 const app = createApp(App);
 
 app.use(router);
